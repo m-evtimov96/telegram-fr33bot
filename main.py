f1b9697145c4123d6a64a940af2c83e2089caa1a
martin-evtimov <m.evtimov196@gmail.com>
2023-01-11 17:12:42 +0200

    Add dad_jokes functionality

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler
 
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_API_KEY")
+DAD_JOKE_TOKEN = os.getenv("DAD_JOKE_KEY")
 WEATHER_TOKEN = os.getenv("WEATHER_API_KEY")
 
 logging.basicConfig(
@@ -37,6 +38,21 @@ async def dog(update, context):
     else:
         dog_photo = data["message"]
         await context.bot.send_photo(chat_id=update.effective_chat.id, photo=dog_photo)
+
+
+async def dad_joke(update, context):
+    request_url = "https://dad-jokes.p.rapidapi.com/random/joke"
+    headers = {
+        "X-RapidAPI-Key": DAD_JOKE_TOKEN,
+        "X-RapidAPI-Host": "dad-jokes.p.rapidapi.com",
+    }
+
+    data = requests.get(url=request_url, headers=headers).json()
+    joke_start = data["body"][0]["setup"]
+    joke_end = data["body"][0]["punchline"]
+
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=joke_start)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=joke_end)
 
 
 async def weather_now(update, context):
@@ -72,10 +88,12 @@ if __name__ == "__main__":
 
     start_handler = CommandHandler("start", start)
     dog_handler = CommandHandler("dog", dog)
+    dad_joke_handler = CommandHandler("dad_joke", dad_joke)
     weather_now_handler = CommandHandler("weather", weather_now)
 
     application.add_handler(start_handler)
     application.add_handler(dog_handler)
+    application.add_handler(dad_joke_handler)
     application.add_handler(weather_now_handler)
 
     application.run_polling(stop_signals=None)
